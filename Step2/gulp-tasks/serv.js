62fe2a17603eb7c00a490da28c4203fc12a309a4
Eugene.Berd <in-spam@ukr.net>
2020-12-08 21:51:50 +0200

    header fixes, hover logo fix

--- Step2/gulp-tasks/serv.js
+++ Step2/gulp-tasks/serv.js
@@ -1,12 +1,11 @@
 const browserSync = require("browser-sync").create();
 
 const serv = () => {
-	browserSync.init({
-		server: {
-			baseDir: "./",
-		},
-		browser: "firefox",
-	});
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
 };
 exports.browsersync = browserSync;
 exports.serv = serv;
