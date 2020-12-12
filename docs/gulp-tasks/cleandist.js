ffc50989e91ca3ed40ed7546ce9932bbac8b6e93
Eugene.Berd <in-spam@ukr.net>
2020-12-12 14:43:01 +0200

    excessive files

--- docs/gulp-tasks/cleandist.js
+++
@@ -1,7 +0,0 @@
-const { src, dest, series, parallel } = require("gulp");
-const del = require("del");
-
-function clean(cb) {
-  del(["./dist/*"], cb());
-}
-exports.clean = clean;
--- docs/gulp-tasks/imagemin.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-
-gulp.task("imgmin", () =>
-  gulp.src("src/img/**").pipe(imagemin()).pipe(gulp.dest("dist/img"))
-);
-
-exports.imgmin = "imgmin";
